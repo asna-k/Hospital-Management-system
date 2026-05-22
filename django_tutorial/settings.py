4473394f8412902003b2e29d93bd0eb4150f59fd
Asna K <asnaalikulangarath@gmail.com>
2026-05-22 12:32:04 +0530

    first commit

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
